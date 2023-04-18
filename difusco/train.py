3735facb18a5929fce20e901417982e155c3a7f8
Junwei Huang <junweih@sa.lti.cs.cmu.edu>
2023-04-17 21:58:02 -0400

    all

--- difusco/train.py
+++ difusco/train.py
@@ -39,8 +39,8 @@ def arg_parser():
 
   parser.add_argument('--diffusion_type', type=str, default='gaussian')
   parser.add_argument('--diffusion_schedule', type=str, default='linear')
-  parser.add_argument('--diffusion_steps', type=int, default=1000)
-  parser.add_argument('--inference_diffusion_steps', type=int, default=1000)
+  parser.add_argument('--diffusion_steps', type=int, default=1024)
+  parser.add_argument('--inference_diffusion_steps', type=int, default=1024)
   parser.add_argument('--inference_schedule', type=str, default='linear')
   parser.add_argument('--inference_trick', type=str, default="ddim")
   parser.add_argument('--sequential_sampling', type=int, default=1)
